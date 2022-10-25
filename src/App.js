279cf1426b8a5a889224ef34359893301177dc9b
flora1532 <flora1532@126.com>
2022-10-25 19:33:11 +0200

    Update App.js
    fjerne smile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! 😋😋😋
+          Hello! 
         </p>
         <a
           className="App-link"
